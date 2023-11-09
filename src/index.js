c17805349678dca058e9cbc8ad1930885f9bb9ab
Wesley Mendes <wesleymendes123321@gmail.com>
2023-11-09 16:56:12 -0300

    Requisito 2

--- src/index.js
+++ src/index.js
@@ -24,7 +24,18 @@ async function readTalkers() {
     const talkers = await fs.readFile(talkerPath);
     return JSON.parse(talkers);
   } catch (error) {
-    res.status(200).json([]);
+    // res.status(200).json([]);
+    console.error(`Arquivo não pôde ser lido: ${error}`);
+  }
+}
+
+async function getTalkerById(id) {
+  try {
+    const talkers = await fs.readFile(talkerPath);
+    const parsedTalkers = JSON.parse(talkers);
+    return parsedTalkers.find((talker) => talker.id === id);
+  } catch (err) {
+    res.status(400).send({ message: 'Pessoa palestrante não encontrada' });
   }
 }
 
@@ -34,5 +45,21 @@ app.get('/talker', async (req, res) => {
     res.status(200).json(talkers);
   } catch (err) {
     res.status(200).json([]);
+  }
+});
+
+app.get('/talker/:id', async (req, res) => {
+  try {
+    const talkers = await readTalkers();
+    const { id } = req.params;
+
+    const talkerById = talkers.find((talker) => talker.id === Number(id));
+    if (!talkerById) {
+      res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+    }
+    return res.status(200).json(talkerById);
+
+  } catch (err) {
+    res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
   }
 });
