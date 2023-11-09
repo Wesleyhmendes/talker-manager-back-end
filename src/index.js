d29624d251774dce4feca8705e2203a0c815f767
Wesley Mendes <wesleymendes123321@gmail.com>
2023-11-09 16:42:14 -0300

    Requisito 1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(express.json());
@@ -13,4 +15,24 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+const talkerPath = path.resolve(__dirname, './talker.json');
+
+async function readTalkers() {
+  try {
+    const talkers = await fs.readFile(talkerPath);
+    return JSON.parse(talkers);
+  } catch (error) {
+    res.status(200).json([]);
+  }
+}
+
+app.get('/talker', async (req, res) => {
+  try {
+    const talkers = await readTalkers();
+    res.status(200).json(talkers);
+  } catch (err) {
+    res.status(200).json([]);
+  }
 });
